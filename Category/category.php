f9b9ebb7e5a390f52f90272c3bd86bf7018c6f37
Anand <148465687+Learner-Anand@users.noreply.github.com>
2024-07-17 15:38:29 +0530

    Update category.php

--- Category/category.php
+++ Category/category.php
@@ -1,8 +1,8 @@
 <?php
-$server_name = "localhost";
-$user_name = "root";
-$password = "";
-$dbname = "sign_in_data";
+$server_name = "bd09ybkhu3grbxzuqmz2-mysql.services.clever-cloud.com";
+$user_name = "ult1ijqhr2eoguyx";
+$password = "9UHn4cWH36BcF382LcKS";
+$dbname = "bd09ybkhu3grbxzuqmz2";
 
 // Create Connection
 $conn = new mysqli($server_name,$user_name,$password,$dbname);
