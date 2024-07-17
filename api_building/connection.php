12bfa8d084be80b21db0d7a604caa9974dfb6c2a
Anand <148465687+Learner-Anand@users.noreply.github.com>
2024-07-17 15:58:39 +0530

    Add files via upload

---
+++ api_building/connection.php
@@ -0,0 +1,53 @@
+
+<?php 
+// connect to mysql database
+$conn = new mysqli("bd09ybkhu3grbxzuqmz2-mysql.services.clever-cloud.com","ult1ijqhr2eoguyx","9UHn4cWH36BcF382LcKS","bd09ybkhu3grbxzuqmz2");
+
+// check connection
+if($conn->connect_error){
+    die("connection failed: ". $conn->connect_error);
+}
+
+$catType = 'bed';
+
+$sql = "SELECT * FROM `category_image`";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($catType == 'search'){
+    while($row = $result->fetch_assoc()){
+        $row["img_src"] = str_replace(array('file/d/','/view?usp=drive_link','/view?usp=sharing'),array('thumbnail?id=','',''),$row["img_src"]);
+        $data[] = $row;
+    }
+}
+else if($catType == 'img_id'){
+    while($row = $result->fetch_assoc()){
+        $data[] = $row['img_id'];
+    }
+}
+else{
+    $sql = "SELECT * FROM `category_image` WHERE `type` LIKE '$catType'";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()){
+        $row["img_src"] = str_replace(array('file/d/','/view?usp=drive_link','/view?usp=sharing'),array('thumbnail?id=','',''),$row["img_src"]);
+        $data[] = $row;
+    }
+}
+// fetch category type from url
+// print_r($catType);
+
+// retrieve data from database
+// $sql = "SELECT * FROM `category_image` WHERE `type` LIKE '$catType'";
+$sql = "SELECT * FROM `category_image`";
+
+//create an array to store the data
+
+// Fetch data and store in array
+
+// Close connection
+$conn->close();
+
+//Output data in JSON format
+echo json_encode($data);
+?>
