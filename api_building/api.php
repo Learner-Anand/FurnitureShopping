9b0a9f36bdb2bebb4b4f150b473a154359a26743
Anand <148465687+Learner-Anand@users.noreply.github.com>
2024-07-17 15:15:15 +0530

    Update api.php

--- api_building/api.php
+++ api_building/api.php
@@ -1,6 +1,6 @@
 <?php 
 // connect to mysql database
-$conn = new mysqli("bd09ybkhu3grbxzuqmz2-mysql.services.clever-cloud.com","bd09ybkhu3grbxzuqmz2","9UHn4cWH36BcF382LcKS","sign_in_data");
+$conn = new mysqli("bd09ybkhu3grbxzuqmz2-mysql.services.clever-cloud.com","ult1ijqhr2eoguyx","9UHn4cWH36BcF382LcKS","bd09ybkhu3grbxzuqmz2");
 
 // check connection
 if($conn->connect_error){
