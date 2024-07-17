f41575bd36f825351ea86d0d86e01f79591b4f7c
Anand <148465687+Learner-Anand@users.noreply.github.com>
2024-07-17 15:28:52 +0530

    Update loginSystem.php

--- api_building/loginSystem.php
+++ api_building/loginSystem.php
@@ -1,6 +1,6 @@
 <?php
 // connect to mysql database
-$conn = new mysqli("localhost","root","","sign_in_data");
+$conn = new mysqli("bd09ybkhu3grbxzuqmz2-mysql.services.clever-cloud.com","ult1ijqhr2eoguyx","9UHn4cWH36BcF382LcKS","bd09ybkhu3grbxzuqmz2");
 
 // check connection
 if($conn->connect_error){
