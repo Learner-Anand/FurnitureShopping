dd7b2bd7844ad09d74606eefcac1cc48fec36e2b
Anand <148465687+Learner-Anand@users.noreply.github.com>
2024-07-17 15:03:04 +0530

    Delete api_building directory

--- api_building/loginSystem.php
+++
@@ -1,9 +0,0 @@
-<?php
-// connect to mysql database
-$conn = new mysqli("localhost","root","","sign_in_data");
-
-// check connection
-if($conn->connect_error){
-    die("connection failed: ". $conn->connect_error);
-}
-?>
